b8a2e71b4b4a5d6a19120597b524a815bc70372c
Luka Koridze <lukakoridze13@gmail.com>
2023-02-03 17:03:33 +0400

    Start Responsive Design

--- JS/main.js
+++ JS/main.js
@@ -1,32 +1,34 @@
-let teamCards = document.querySelectorAll(".card");
-teamCards.forEach((card, index) => {
-  if (index == 0) {
-    card.style.left = "0px";
-  } else if (index == 1) {
-    card.style.left = "50%";
-  } else {
-    card.style.left = "350px";
-  }
-});
-
-setInterval(() => {
+if (window.innerWidth > 480) {
+  // cards animation
   let teamCards = document.querySelectorAll(".card");
-
-  teamCards.forEach((card) => {
-    card.style.zIndex = "0";
-    card.style.opacity = "0.8";
-    if (card.style.left === "0px") {
-      card.style.left = "350px";
-    } else if (card.style.left === "350px") {
-      card.style.top = "50px";
-      card.style.left = "50%";
-      card.style.zIndex = "10";
-      card.style.transform = "translateX(-50%)";
-      card.style.opacity = "1";
-    } else {
-      card.style.top = "0px";
+  teamCards.forEach((card, index) => {
+    if (index == 0) {
       card.style.left = "0px";
-      card.style.transform = "translateX(0)";
+    } else if (index == 1) {
+      card.style.left = "50%";
+    } else {
+      card.style.left = "350px";
     }
   });
-}, 4000);
+  setInterval(() => {
+    let teamCards = document.querySelectorAll(".card");
+
+    teamCards.forEach((card) => {
+      card.style.zIndex = "0";
+      card.style.opacity = "0.8";
+      if (card.style.left === "0px") {
+        card.style.left = "350px";
+      } else if (card.style.left === "350px") {
+        card.style.top = "50px";
+        card.style.left = "50%";
+        card.style.zIndex = "10";
+        card.style.transform = "translateX(-50%)";
+        card.style.opacity = "1";
+      } else {
+        card.style.top = "0px";
+        card.style.left = "0px";
+        card.style.transform = "translateX(0)";
+      }
+    });
+  }, 4000);
+}
