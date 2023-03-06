ff7b0164891be0a675c89bb4ee850b618a6f62b1
Luka Koridze <lukakoridze13@gmail.com>
2023-03-06 14:57:30 +0400

    Fares

--- dashboard.js
+++ dashboard.js
@@ -1,26 +1,47 @@
 document.getElementById("sign_out").addEventListener("click", () => {
   localStorage.setItem("Nomadao_Login", "false");
 });
-
 const NFT = localStorage.getItem("Nomadao_Token");
 const ADDRESS = localStorage.getItem("Nomadao_Address");
 const BALANCE = localStorage.getItem("Nomadao_Balance") || 3000;
 const MOON = document.querySelector(".fa-moon");
 const SUN = document.querySelector(".fa-sun");
+const ASIDE = document.querySelector("aside");
+const TOP = document.querySelector(".top");
+
+document.querySelector('.yourNFT').classList.add(localStorage.getItem("Nomadao_Token"))
+document.querySelector('.you_own_img').src = './Images/'+localStorage.getItem("Nomadao_Token")+".png"
+document.querySelector('.you_own').innerText = "You own " + localStorage.getItem("Nomadao_Token").toUpperCase() + " NFT"
+
+ASIDE.addEventListener("mouseover", () => {
+  ASIDE.classList.add("active");
+  TOP.style.paddingLeft = "240px";
+});
+ASIDE.addEventListener("mouseleave", () => {
+  ASIDE.classList.remove("active");
+  TOP.style.paddingLeft = "120px";
+});
+
 MOON.addEventListener("click", () => {
   themeChange("dark");
 });
 SUN.addEventListener("click", () => {
   themeChange("light");
 });
-themeChange(localStorage.getItem("Nomadao_Theme"));
+if (localStorage.getItem("Nomadao_Theme") === "light") {
+  SUN.classList.remove("active");
+  MOON.classList.add("active");
+} else {
+  MOON.classList.remove("active");
+  SUN.classList.add("active");
+}
+
 setBalance(BALANCE);
 
-
-
-
 function setBalance(num) {
-  document.getElementById("balance").innerText = "$ " + num;
+  num =  Number(num)
+  document.getElementById("balance").innerText = "$ " + num.toFixed(2);
+  localStorage.setItem("Nomadao_Balance", num)
 }
 
 function themeChange(theme) {
